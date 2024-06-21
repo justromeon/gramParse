b946396e7464af561ad7c86e59071191670df199
Justin Kian Romeon <92698759+justromeon@users.noreply.github.com>
2024-06-21 20:38:15 +0800

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -6,28 +6,19 @@ import Data.Aeson (encodeFile)
 import System.IO (hFlush, stdout)
 import Parser (play)
 
-
--- Main function
 main :: IO ()
 main = do
-    -- Prompt user for input file name
     putStr "Enter the name of the input text file (inside 'input' directory): "
     hFlush stdout
     inputFileName <- getLine
-    
-    -- Read input file content
     input <- readFile $ "input/" ++ inputFileName
     
-    -- Parse input using the Parser module
     case play input of
         Left err -> putStrLn $ "Parse error: " ++ show err
         Right parsed -> do
-            -- Prompt user for output file name
             putStr "Enter the name for the output JSON file (inside 'output' directory): "
             hFlush stdout
             outputFileName <- getLine
             
-            -- Write parsed JSON to output file
             encodeFile ("output/" ++ outputFileName) parsed
-            
             putStrLn $ "Successfully parsed and wrote JSON to: output/" ++ outputFileName
